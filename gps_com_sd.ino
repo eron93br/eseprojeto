e4e1f41b383dc91d415cac2a37d40b515c9a55eb
eronides neto <eron93@gmail.com>
2017-05-30 13:35:42 -0300

    Create gps_com_sd.ino

---
+++ gps_com_sd.ino
@@ -0,0 +1,132 @@
+
+
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+/*
+ This example uses software serial and the TinyGPS++ library by Mikal Hart
+ Based on TinyGPSPlus/DeviceExample.ino by Mikal Hart
+ Modified by acavis
+*/
+
+// Choose two Arduino pins to use for software serial
+// The GPS Shield uses D2 and D3 by default when in DLINE mode
+int RXPin = 2;
+int TXPin = 4;
+
+// The Skytaq EM-506 GPS module included in the GPS Shield Kit
+// uses 4800 baud by default
+int GPSBaud = 4800;
+
+// Create a TinyGPS++ object called "gps"
+TinyGPSPlus gps;
+
+// Create a software serial port called "gpsSerial"
+SoftwareSerial gpsSerial(RXPin, TXPin);
+
+void setup()
+{
+  // Start the Arduino hardware serial port at 9600 baud
+  Serial.begin(9600);
+  pinMode(10, OUTPUT);
+    if (!SD.begin(10)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  // Start the software serial port at the GPS's default baud
+  gpsSerial.begin(GPSBaud);
+
+  Serial.println(F("DeviceExample.ino"));
+  Serial.println(F("A simple demonstration of TinyGPS++ with an attached GPS module"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+}
+
+void loop()
+{
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (gpsSerial.available() > 0)
+    if (gps.encode(gpsSerial.read()))
+      displayInfo();
+
+  // If 5000 milliseconds pass and there are no characters coming in
+  // over the software serial port, show a "No GPS detected" error
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected"));
+    while(true);
+  }
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+    Serial.print(F(" Velocidade: "));
+    Serial.print(gps.speed.mph(), 2);
+    Serial.print(F(" Degree: "));
+    Serial.print(gps.course.deg(),2);
+    myFile = SD.open("test.txt", FILE_WRITE);
+    
+    if (myFile) 
+    {
+       myFile.println(" ");
+       myFile.print(gps.location.lat(),6);
+       myFile.print(" , ");
+       myFile.print(gps.location.lng(),6);
+       myFile.print(" , ");
+       myFile.print(gps.speed.mph(), 2);
+      // close the file:
+       myFile.close();          
+     }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
